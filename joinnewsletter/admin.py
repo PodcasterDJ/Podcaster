28534a781aa69caafe1ccf4849485b91d332ef86
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-03-27 17:28:51 +0100

    Admin emails

--- joinnewsletter/admin.py
+++ joinnewsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Join
+
+class JoinAdmin(admin.ModelAdmin):
+    list_display = ('email', 'timestamp')
+admin.site.register(Join, JoinAdmin)
