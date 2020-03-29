0fc1d34127f75156715b59649b1b1dccebb9c664
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-03-29 17:09:19 +0200

    Latest posts in footer

--- podcasts/context_processors.py
+++ podcasts/context_processors.py
@@ -1,8 +1,11 @@
-from episodes.models import Episode, Tags
+from episodes.models import Episode, Tags, Category
+from blog.models import Post
 
 def latest_episodes(request):
     return {
         "latest_episodes": Episode.objects.filter(status='published')[0:3],
-        "tags": Tags.objects.all()
+        "latest_posts": Post.objects.filter(status='published')[0:3],
+        "tags": Tags.objects.all(),
+        "categories": Category.objects.all(),
     }
 
