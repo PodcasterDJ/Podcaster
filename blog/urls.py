5e00e1e420011c46e843caf30c70bc0929c606d5
lokeshramanav <lokeshmay3@gmail.com>
2020-04-18 13:35:01 +0800

    Added api for GET post and GET post/:d

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import BlogListView, BlogDetailView
+from .apiviews import PostsApiViews
+
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
     path('/post/<detail_post_slug>', BlogDetailView.as_view(), name='post_detail'),
-
+    path('api/v1/post/<int:id>', PostsApiViews.as_view())
 ]
