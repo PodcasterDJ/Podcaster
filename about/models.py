f8bf473aaf57944b9a8ce4d0e9b81e6e4bdd3ccf
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-05-01 03:16:24 +0200

    Changing about model

--- about/models.py
+++ about/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.utils import timezone
 
-class GeneralInfo(models.Model):
+INFO_ELEMENT = (
+    ('select', 'SELECT'),
+    ('podcast_description', 'PODCAST_DESCRIPTION'),
+    ('profile_description', 'PROFILE_DESCRIPTION'),
+    ('patreon', 'PATREON'),
+    ('paypal', 'PAYPAL'),
+)
 
-    podcast_description = models.TextField(blank=True, null=True)
-    personal_description = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True,
-                                  null=True, help_text="This picture will be uploaded to AWS.")
+
+class GeneralInfo(models.Model):
+    name = models.TextField(
+        max_length=500, choices=INFO_ELEMENT, default='select', unique=True)
+
+    # TODO: Extend User model and enrich it with photo option.
+    # profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True,
+    #                               null=True, help_text="This picture will be uploaded to AWS.")
     updated = models.DateTimeField(auto_now=True)
-    publish = models.DateTimeField(default=timezone.now)
-    patreon_link = models.CharField(max_length=250, null=True, blank=True)
-    paypal_email = models.EmailField( null=True, blank=True)
 
     def __str__(self):
-        return '{}'.format(self.updated)
+        return '{}'.format(self.name)
