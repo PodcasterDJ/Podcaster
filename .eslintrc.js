5bc7e0ecf14c3f88e078d047ab4a3cc3b0596b50
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-05-01 05:49:06 +0200

    EsLintConfig, Added audio component to main page.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,28 +3,18 @@ module.exports = {
     env: {
         node: true
     },
-    eslintConfig: {
-        root: true,
-        extends: [
-            "plugin:vue/essential",
-            "plugin:prettier/recommended",
-            "eslint:recommended"
-        ]
-    },
+    extends: [
+        "plugin:vue/essential",
+        "plugin:prettier/recommended",
+        "eslint:recommended"
+    ],
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
         "global-require": "off",
         "no-new": 0, // Intrusive when using Chart.js instances.
         "no-underscore-dangle": 0, // Chart.js uses underscore dangles (_) internally.
-        "import/no-unresolved": 0, // False positives regarding imports that use aliases.
-        "vue/script-indent": [
-            "warn",
-            2,
-            {
-                baseIndent: 1
-            }
-        ]
+        "import/no-unresolved": 0, // False positives regarding imports that use aliases. 
     },
     overrides: [
         {
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,33 +1,31 @@
 <template>
-  <div>
-    <nav-bar-basic></nav-bar-basic>
-    <jumbotron-basic></jumbotron-basic>
-    <b-container>
-      <carousel-new></carousel-new>
-    </b-container>
-    <b-container>
-      <b-row class="text-center justify-content-center">
-        <card-basic></card-basic>
-        <card-basic></card-basic>
-        <card-basic></card-basic>
-      </b-row>
-    </b-container>
-  </div>
+    <div>
+        <nav-bar-basic></nav-bar-basic>
+        <jumbotron-basic></jumbotron-basic>
+
+        <b-container>
+            <b-row class="text-center justify-content-center">
+                <card-audio></card-audio>
+                <b-container>
+                    <carousel-new></carousel-new>
+                </b-container>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 
 <script>
-import CarouselNew from '@/components/carousel/CarouselNew.vue';
-import CardBasic from '@/components/cards/CardBasic.vue';
-import NavBarBasic from '@/components/navbars/NavBarBasic.vue';
-import JumbotronBasic from '@/components/jumbotrons/JumbotronBasic.vue';
+import CarouselNew from "@/components/carousel/CarouselNew.vue";
+import CardAudio from "@/components/cards/CardAudio.vue";
+import NavBarBasic from "@/components/navbars/NavBarBasic.vue";
+import JumbotronBasic from "@/components/jumbotrons/JumbotronBasic.vue";
 
 export default {
-  components: {
-    CarouselNew,
-    CardBasic,
-    NavBarBasic,
-    JumbotronBasic,
-  },
+    components: {
+        CarouselNew,
+        CardAudio,
+        NavBarBasic,
+        JumbotronBasic
+    }
 };
 </script>
-
